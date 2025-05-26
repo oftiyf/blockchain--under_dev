e3c8a7f6b3dbbb664b89093232e90ad778b08811
oftiyf <1486642424@qq.com>
2025-05-26 19:31:50 +0800

    fixed:feature

--- common/hash.go
+++ common/hash.go
@@ -2,6 +2,9 @@ package common
 
 import (
 	"encoding/hex"
+
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr"
+	"github.com/consensys/gnark-crypto/ecc/bn254/fr/mimc"
 )
 
 const (
@@ -11,12 +14,22 @@ const (
 type Hash [HashLength]byte
 
 func (h Hash) NewHash(data []byte) Hash {
-	if len(data) != HashLength {
-		panic("data length must be 32")
-	}
-	var hash Hash
-	copy(hash[:], data)
-	return hash
+	// 将字节数据转换为域元素
+	var input fr.Element
+	input.SetBytes(data)
+
+	// 创建 MiMC 哈希实例
+	mimcHash := mimc.NewMiMC()
+
+	// 计算哈希
+	inputBytes := input.Bytes()
+	mimcHash.Write(inputBytes[:])
+	hash := mimcHash.Sum(nil)
+
+	// 转换为 Hash
+	var result Hash
+	copy(result[:], hash)
+	return result
 }
 
 func (h Hash) Bytes() []byte {
